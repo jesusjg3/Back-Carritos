19aa7252c2451c1de890bdf7f7f3b8d593544504
jesusjg3 <maciaslugojesusfernando@gmail.com>
2026-02-04 00:36:18 -0500

    xd

--- app/Events/TripLocationUpdated.php
+++ app/Events/TripLocationUpdated.php
@@ -6,6 +6,7 @@ use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
+
 use Illuminate\Queue\SerializesModels;
 
 class TripLocationUpdated implements ShouldBroadcastNow
